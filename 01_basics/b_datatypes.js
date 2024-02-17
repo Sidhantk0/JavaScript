ccb1ba4e8f37091f10bad505edfcdba8b7e639d7
Sidhantk0 <114770907+Sidhantk0@users.noreply.github.com>
2024-02-17 11:28:05 +0000

    differnet type of datatypes

---
+++ 01_basics/b_datatypes.js
@@ -0,0 +1,17 @@
+/*
+datatypes defined during let const and var
+number
+bigint
+string
+boolean
+null => considered to be of type object
+undefined => have type undefined
+symbol
+object
+typeof => to find the type
+
+*/
+
+let a ="sid";
+
+console.log(typeof a);
