06e62871a98702d1d8c56dd4eadc662e1a627ef6
Sidhantk0 <114770907+Sidhantk0@users.noreply.github.com>
2024-02-17 13:26:47 +0000

    datatype conversions

---
+++ 01_basics/c_dataConversion.js
@@ -0,0 +1,25 @@
+let name1 = "rahu";
+console.log(typeof name1);
+
+let scoreinString = Number(name1);
+console.log(typeof scoreinString);
+console.log(scoreinString);
+
+/*
+Output of the above: 
+string
+string
+number
+NaN
+
+"33" => 33 when string to number conversion is done
+"33sfs" => NaN Because trying to convert string to Number
+true => 1, false => 0
+
+*/
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(isLoggedIn); // output will be 0
+console.log(booleanIsLoggedIn); // output will be false
